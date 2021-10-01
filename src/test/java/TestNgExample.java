896c1b64fd614a917746b012b275250d992d18af
Nandkishor Periwal <nkperiwal@gmail.com>
2021-10-02 00:54:28 +0530

    Add Selenium Tests

---
+++ src/test/java/TestNgExample.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNgExample {
+
+    @Test()
+    public void testSummation() {
+        int sum = 2 + 2;
+        Assert.assertEquals(sum, 4);
+
+    }
+
+}
